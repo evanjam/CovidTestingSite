87bfef9594d9e36b95e0d12f97260e62d1cf2897
Thatcher Deyoe <tdeyoe2@gmail.com>
2021-09-21 10:54:26 -0400

    making login page connect to db

---
+++ includes/authenticate.php
@@ -0,0 +1,54 @@
+<?php
+session_start();
+//variables to connect to user login
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'sc19_alpha';
+// use to connect to the table
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	// If error accurs with connecting to the database then display message
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+//isset() will check to make sure data is defined and 
+if ( !isset($_POST['username'], $_POST['password']) ) {
+	// data was not secured and could not be sent 
+	exit('Please fill both the username and password fields!');
+}
+
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+if ($stmt = $con->prepare('SELECT id, password FROM user_login WHERE username = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+	$stmt->store_result();
+
+    if ($stmt->num_rows > 0) {
+        $stmt->bind_result($id, $password);
+        $stmt->fetch();
+        // Account exists, now we verify the password.
+        // Note: remember to use password_hash in your registration file to store the hashed passwords.
+        if (password_verify($_POST['password'], $password)) {
+            // Verification success! User has logged-in!
+            // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
+            session_regenerate_id();
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $_POST['username'];
+            $_SESSION['id'] = $id;
+            echo 'Welcome ' . $_SESSION['name'] . '!';
+        } else {
+            // Incorrect password
+            echo 'Incorrect username and/or password!';
+        }
+    } else {
+        // Incorrect username
+        echo 'Incorrect username and/or password!';
+    }
+
+
+	$stmt->close();
+}
+?>
