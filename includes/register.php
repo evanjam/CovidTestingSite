1ab9204c580064a020c09a6002e92c3a9850a99e
Dustin1100 <Dustin1100@gmail.com>
2021-09-21 19:53:04 -0400

    Fixed path
    Fixed path from register to config

--- includes/register.php
+++ includes/register.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	include('includes/config.php');
+	include('../includes/config.php');
 	if (isset($_POST['register'])) {
 		$username = $_POST['username'];
 		$password = $_POST['password'];
