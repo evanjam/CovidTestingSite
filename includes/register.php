08c4c77a0a4f2ea3b3ca960bfd27362e87ad7e7d
evanjam <deadhead9420@gmail.com>
2021-09-23 22:00:36 -0400

    update register.php
    trimming some fat away from register.php, still doesnt work

--- includes/register.php
+++ includes/register.php
@@ -5,15 +5,15 @@
 	if (isset($_POST['register'])) {
 		$username = $_POST['username'];
 		$password = $_POST['password'];
-		echo "registration form triggered. you entered:<br>username:$username<br>password:$password<br>nothing else works so good luck :)";
 		$password_hash = password_hash($password, PASSWORD_BCRYPT);
-		$query = $connection->prepare("SELECT * FROM user_login WHERE username=:username");
-		$query->bindParam("username", $username, PDO::PARAM_STR);
-		$query->execute();
-		if ($query->rowCount() > 0) {
-			echo '<p class="error">user already registered</p>';
-		}
-		if ($query->rowCount() == 0) {
+		echo "registration form triggered. you entered:<br>username:$username<br>password:$password<br>password hash:$password_hash<br>";
+//		$query = $connection->prepare("SELECT * FROM user_login WHERE username=:username");
+//		$query->bindParam("username", $username, PDO::PARAM_STR);
+//		$query->execute();
+//		if ($query->rowCount() > 0) {
+//			echo '<p class="error">user already registered</p>';
+//		}
+//		if ($query->rowCount() == 0) {
 			$query = $connection->prepare("INSERT INTO user_login(username, password) VALUES (:username,:password_hash)");
 			$query->bindParam("username", $username, PDO::PARAM_STR);
 			$query->bindParam("password_hash", $password_hash, PDO::PARAM_STR);
@@ -21,8 +21,8 @@
 			if ($result) {
 				echo '<p class="success">successful registration</p>';
 			} else {
-				echo '<p class="error">registration error</p>';
+				echo '<p class="error">registration error you suck</p>';
 			}
-		}
+//		}
 	}
 ?>
