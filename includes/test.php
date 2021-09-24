b21576232cc99da95565771e25bcf9b45af64c64
evanjam <deadhead9420@gmail.com>
2021-09-23 22:26:21 -0400

    test password_hash function and update db
    localhost/covidtestingsite/includes/test.php to study the usage of the password_hash method
    +also updates db, changing password to a varchar(64) in user_login
    +also re-organizes sql folder, creating old folder for older db copies

--- includes/test.php
+++ includes/test.php
@@ -15,4 +15,8 @@
 		echo "this works, which means that password_verify is decrypting the password stored in \"\$password_hash\"";
 	} else
 		echo "you really suck";
+	
+	echo "<br><br><br>";
+	echo "this is really interesting because every time you refresh this page and re-run this code, the password hash is different<br>";
+	echo "is this going to make storing and reading passwords difficult?";
 ?>
---
+++ sql/sc19_alpha_current_9-23.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2021 at 04:20 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sc19_alpha`
+--
+CREATE DATABASE IF NOT EXISTS `sc19_alpha` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `sc19_alpha`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login_log`
+--
+
+CREATE TABLE `login_log` (
+  `LLID` int(11) NOT NULL,
+  `ULID` int(11) NOT NULL,
+  `login_date` varchar(25) NOT NULL,
+  `is_successful` int(1) NOT NULL,
+  `error_message` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_login`
+--
+
+CREATE TABLE `user_login` (
+  `ULID` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_permission`
+--
+
+CREATE TABLE `user_permission` (
+  `UPEID` int(11) NOT NULL,
+  `ULID` int(11) NOT NULL,
+  `is_admin` int(1) NOT NULL,
+  `is_doctor` int(1) NOT NULL,
+  `is_vendor` int(1) NOT NULL,
+  `is_employee` int(1) NOT NULL,
+  `is_patient` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login_log`
+--
+ALTER TABLE `login_log`
+  ADD PRIMARY KEY (`LLID`),
+  ADD KEY `ULID` (`ULID`);
+
+--
+-- Indexes for table `user_login`
+--
+ALTER TABLE `user_login`
+  ADD PRIMARY KEY (`ULID`);
+
+--
+-- Indexes for table `user_permission`
+--
+ALTER TABLE `user_permission`
+  ADD PRIMARY KEY (`UPEID`),
+  ADD KEY `ULID` (`ULID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login_log`
+--
+ALTER TABLE `login_log`
+  MODIFY `LLID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_login`
+--
+ALTER TABLE `user_login`
+  MODIFY `ULID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_permission`
+--
+ALTER TABLE `user_permission`
+  MODIFY `UPEID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `login_log`
+--
+ALTER TABLE `login_log`
+  ADD CONSTRAINT `login_log_ibfk_1` FOREIGN KEY (`ULID`) REFERENCES `user_login` (`ULID`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_permission`
+--
+ALTER TABLE `user_permission`
+  ADD CONSTRAINT `user_permission_ibfk_1` FOREIGN KEY (`ULID`) REFERENCES `user_login` (`ULID`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
