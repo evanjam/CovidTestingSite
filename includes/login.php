385d439cf679e06dbff4c8319b027164836a206f
evanjam <deadhead9420@gmail.com>
2021-09-25 01:43:59 -0400

    registration now checks to make sure username doesnt already exist
    login still work in progress

--- includes/login.php
+++ includes/login.php
@@ -7,6 +7,22 @@
     if (isset($_POST['login'])) {
         $username = $_POST['username'];
         $password = $_POST['password'];
-
+		
+		$sql_statement = "SELECT * FROM user_login WHERE username = '$username'";
+		$result = $connect->query($sql_statement);
+		if($result->num_rows > 0) {
+			echo "user exists..do something..";
+			$sql_statement2 = "SELECT password FROM user_login WHERE username = '$username'";
+			$stored_password = $connect->query($sql_statement2);
+			$validate_password = password_verify($password, $stored_password);
+			if($validate_password ) {
+				echo "no way....????";
+			} else
+				echo "lol of course it didnt work";
+		} else {
+			echo "user doesnt exist, redirecting to home..";
+			header('Refresh: 1;URL=../index.php');
+		}
+		
 	}
 ?>
--- includes/register.php
+++ includes/register.php
@@ -8,13 +8,23 @@
 		$username = $_POST['username']; //saves variables from the user's input
 		$password = $_POST['password'];
 		$password_hash = password_hash($password, PASSWORD_BCRYPT); //prepares the password hash
-
-		$sql_statement = "INSERT INTO user_login (username, password) VALUES ('$username', '$password_hash')"; //prepare sql insertion statement
 		
-		if($connect->query($sql_statement) === TRUE) { //call query function on $connect and pass $sql_statement as parameter, then does an if statement to check if it worked at the same time
-			echo "new record created";
-		} else 
-			echo "error";
-		$connect->close(); //it still works if I don't include this but I feel like it's probably necessary down the line to do this
+		$sql_statement1 = "SELECT * FROM user_login WHERE username = '$username'"; //prepares query statement to check if username already exists
+		$result = $connect->query($sql_statement1); //runs $sql_statement1 query and stores the result in $result
+		
+		if($result->num_rows > 0) { //if $result > 0 returns true, then there exists rows where the username=$username 
+			echo "username already exists, redirecting to home..."; //print a message saying that username already exists
+			header('Refresh: 1;URL=../index.php'); //wait 1 second and refresh index.php homepage
+		} else { //if username doesnt already exist in database, execute remaining steps to insert the username and password
+			
+			$sql_statement2 = "INSERT INTO user_login (username, password) VALUES ('$username', '$password_hash')"; //prepare sql insertion statement
+		
+			if($connect->query($sql_statement2) === TRUE) { //call query function on $connect and pass $sql_statement as parameter, then does an if statement to check if it worked at the same time
+				echo "new record created, redirecting to home...";
+				header('Refresh: 1;URL=../index.php');
+			} else 
+				echo "error";
+			$connect->close(); //it still works if I don't include this but I feel like it's probably necessary down the line to do this
+		}
 	}
 ?>
