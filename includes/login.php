84308b5f8151685211db790b0541e192a302ec73
evanjam <deadhead9420@gmail.com>
2021-09-26 19:35:16 -0400

    login.php working
    yay

--- includes/login.php
+++ includes/login.php
@@ -1,20 +1,21 @@
 <?php
 //login.php
 //https://stackoverflow.com/questions/46819734/how-to-check-username-and-password-matches-the-database-values
-//work in progress evan/dustin 9/26
+//https://stackoverflow.com/questions/26536293/php-password-hash-password-verify
 
     include('connect.php'); 
 	
     if (isset($_POST['login'])) {
         $username = $_POST['username']; 
         $password = $_POST['password'];
-		$password_hash = password_hash($password, PASSWORD_BCRYPT); 
+//		$password_hash = password_hash($password, PASSWORD_BCRYPT); 
 		$get_user = "SELECT * FROM user_login WHERE username = '$username'"; 
 		$result = $connect->query($get_user); 
 		
 		if($result->num_rows > 0) { 
 			$get_password = "SELECT password FROM user_login WHERE username = '$username'"; 
-			if(strcmp($password_hash, $get_password)) {
+			$row = $result->fetch_array(MYSQLI_ASSOC);
+		if(password_verify($password, $row['password'])) {
 				echo "credentials match, username and password has been successfully verified!";
 			} else {
 				echo "credentials do not match what is stored in the db, try again";
