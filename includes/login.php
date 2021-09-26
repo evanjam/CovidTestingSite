8ae483269b23a76145bca4f699d11d4163dcf42e
evanjam <deadhead9420@gmail.com>
2021-09-26 12:26:37 -0400

    small changes, login.php
    login.php
    -moved some lines around, formatting and making it easier to read
    -changed some echo statements for easier debugging
    -dustin and evan need to convene on this because I discovered that password verification is not working correctly

--- includes/login.php
+++ includes/login.php
@@ -1,29 +1,25 @@
 <?php
 //login.php
 //https://stackoverflow.com/questions/46819734/how-to-check-username-and-password-matches-the-database-values
+//work in progress evan/dustin 9/26
 
-    include('connect.php');
+    include('connect.php'); 
 	
     if (isset($_POST['login'])) {
-        $username = $_POST['username'];
+        $username = $_POST['username']; 
         $password = $_POST['password'];
-		$password_hash = password_hash($password, PASSWORD_BCRYPT);
-
-		$get_user = "SELECT * FROM user_login WHERE username = '$username'";
-		$result = $connect->query($get_user);
-		if($result->num_rows > 0) {
-			echo "user exists..do something..";
-			
-			//my attempt at retrieving the password stored in the database 
-			//and then using the password_verify function to compare it to the hashed user's input
-			$get_password = "SELECT password FROM user_login WHERE username = '$username'";
-			//$stored_password = $connect->query($sql_statement2);
-			//$validate_password = password_verify($password_hash, $stored_password); 
-			
+		$password_hash = password_hash($password, PASSWORD_BCRYPT); 
+		$get_user = "SELECT * FROM user_login WHERE username = '$username'"; 
+		$result = $connect->query($get_user); 
+		
+		if($result->num_rows > 0) { 
+			$get_password = "SELECT password FROM user_login WHERE username = '$username'"; 
 			if(strcmp($password_hash, $get_password)) {
-				echo "no way....????";
-			} else
-				echo "lol of course it didnt work";
+				echo "credentials match, username and password has been successfully verified!";
+			} else {
+				echo "credentials do not match what is stored in the db, try again";
+				header('Refresh: 1;URL=../index.php');
+			}
 		} else {
 			echo "user doesnt exist, redirecting to home..";
 			header('Refresh: 1;URL=../index.php');
