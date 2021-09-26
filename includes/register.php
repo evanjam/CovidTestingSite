662e36eb434db09c59590cbbe322a90414df31bd
evanjam <deadhead9420@gmail.com>
2021-09-26 12:25:57 -0400

    small changes, no code changes
    register.php
    -update a couple comments
    -add my query line back, commented out, for people to look at and decide which type of query to use
    
    login.php
    -moved some lines around, formatting and making it easier to read
    -changed some echo statements for easier debugging
    -dustin and evan need to convene on this because I discovered that password verification is not working correctly

--- includes/register.php
+++ includes/register.php
@@ -8,14 +8,17 @@
 		$username = $_POST['username']; //saves variables from the user's input
 		$password = $_POST['password'];
 		$password_hash = password_hash($password, PASSWORD_BCRYPT); //prepares the password hash
-		$select_user = "SELECT * FROM user_login WHERE username = '$username'"; //prepares query statement to check if username already exists
-		$result = $connect->query($select_user); //runs $sql_statement1 query and stores the result in $result
+		$select_user = "SELECT * FROM user_login WHERE username = '$username'"; //prepares sql statement to check if username already exists
+		$result = $connect->query($select_user); //runs $select_user as a query and stores the result in $result
 		
-		if($result->num_rows > 0) { //if $result > 0 returns true, then there exists rows where the username=$username 
+		if($result->num_rows > 0) { //if $result->num_rows > 0 returns true, then there exists rows where the username=$username 
 			echo "username already exists, redirecting to home..."; //print a message saying that username already exists
 			header('Refresh: 1;URL=../index.php'); //wait 1 second and refresh index.php homepage
 		} else { //if username doesnt already exist in database, execute remaining steps to insert the username and password
 			$insert_user = "INSERT INTO user_login (ULID, username, password) VALUES (NULL, '$username', '$password_hash')"; //prepare sql insertion statement
+			
+//			if($connect->query($sql_statement2) === TRUE) { //evan's query function, up for discussion on which to use
+			
 			if(mysqli_query($connect, $insert_user)) { //call query function on $connect and pass $sql_statement as parameter, then does an if statement to check if it worked at the same time
 				echo "new record created, redirecting to home...";
 				header('Refresh: 1;URL=../index.php');
