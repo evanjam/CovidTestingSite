c285bfeb28a00d08f3fcdf37ef2fe7c065fc7291
evanjam <deadhead9420@gmail.com>
2021-10-02 00:31:50 -0400

    Update getTests.php
    -an example of querying the test_sample table for tests on the user defined date
    -only works if you've inserted a row into test_sample db (have to manually insert from phpmyadmin)

--- includes/getTests.php
+++ includes/getTests.php
@@ -1,6 +1,18 @@
 <?php
 
-//    include('connect.php'); 
+    include('connect.php'); 
 	
-//	}
+	if (isset($_POST['labtests'])) {
+		$date = $_POST['date'];
+		
+		$query_date = "SELECT * FROM `test_sample` WHERE `test_date` = '$date'";
+		$result = $connect->query($query_date);
+		
+		if($result->num_rows > 0) {
+			echo 'test exists on that date';
+		} else {
+			echo 'gay';
+		}
+	}
+	
 ?>
