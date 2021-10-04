3dc39999c382b986d23987dfb569668b9c1fa411
evanjam <deadhead9420@gmail.com>
2021-10-04 12:06:43 -0400

    begin working on submit_test backend

--- includes/submit_test.php
+++ includes/submit_test.php
@@ -8,8 +8,15 @@
 		$ssn = $_POST['ssn'];
 		$serial = $_POST['serial'];
 		
+		$select_user = "SELECT * FROM user_profile WHERE ssn = '$ssn'";
+		$result = $connect->query($select_user);
 		
-		
+		if($result->num_rows > 0) { //if $result->num_rows > 0 returns true, then there exists rows where the username=$username 
+			echo "user exists, do stuf";
+		} else {
+			echo "no patient exists with the ssn that was specified. please register a new patient before submitting their test.";
+			header('Refresh: 2;URL=../forms/employee/employee_dashboard.php');
+		}
 	}
 	
 ?>
