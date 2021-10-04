0aff15161a49f2985b50e029dc2f6fbf93145683
Thatcher Deyoe <tdeyoe2@gmail.com>
2021-10-04 19:18:40 -0400

    changed from getTests back to lab getTests

--- forms/lab/lab_dashboard.php
+++ forms/lab/lab_dashboard.php
@@ -17,7 +17,7 @@
 
     <div class="getTests">
         <h1>Enter date for tests </h1>
-        <form method="post" action="../../includes/getTests.php" name="labtests">
+        <form method="post" action="../../includes/lab_getTests.php" name="labtests">
             <input type="text" name="date"  placeholder="Desired date (year-month-day)" id="date" required />
             <input type="submit" name="labtests" value="Search">
         </form>
