b295b79ed134d73e17ee6af54aaa317f0cb224f9
Dustin1100 <Dustin1100@gmail.com>
2021-10-03 21:55:11 -0400

    Added the patient dashboard
    Added the bones for the patient dashboard

---
+++ forms/patient/patient_dashboard.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patient dashboard</title>
+    <link href="../../css/dashboard.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+    <div class="header">
+        <h1>Patient dashboard</h1>
+    
+    </div>
+
+
+    <div class="getTests">
+        <h1>Test Results </h1>
+        <form method="post" action="../../includes/lab_getTests.php" name="labtests">
+        </form>
+    </div>
+
+    <div>(Note: Display test results if is_signed === true)</div>
+    <table class="desiredResults" id="desiredResults">
+        <tr>
+            <th>Test Date</th>
+            <th>Result</th>
+        <tr>
+        <tr>
+            <td>A</td>
+            <td>B</td>
+        <tr>
+
+    </table>
+
+
+ 
+
+</body>
+</html>
