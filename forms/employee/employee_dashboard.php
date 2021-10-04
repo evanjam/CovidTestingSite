0cdbe5ec5c01f05a4507f04ec43e0714feb53b7d
evanjam <deadhead9420@gmail.com>
2021-10-03 22:17:02 -0400

    Create employee_dashboard.php
    a

---
+++ forms/employee/employee_dashboard.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee dashboard</title>
+    <link href="../../css/dashboard.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+    <div class="header">
+        <h1>Employee dashboard</h1>
+    
+    </div>
+    
+    <div><a href="employee_register.php">Link to register patient</a></div>
+    <br>
+    <div>(Link to register new test)</div>
+
+
+</body>
+</html>
