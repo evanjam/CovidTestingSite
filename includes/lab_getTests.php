a181d628a1d3f60658c54ad317a19281b9ad653e
evanjam <deadhead9420@gmail.com>
2021-10-04 20:16:35 -0400

    idea to try to display results on dashboard front end
    -the lab_getTests.php was updated with the entire lab_dashboard front end

--- includes/lab_getTests.php
+++ includes/lab_getTests.php
@@ -22,4 +22,55 @@
 	}
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lab dashboard</title>
+    <link href="../css/dashboard.css" rel="stylesheet" type="text/css">
+</head>
+<body>
 
+    <div class="header">
+        <h1>Lab user dashboard</h1>
+    
+    </div>
+	
+	<div><a href="../forms/lab/lab_dashboard.php">Home</a></div>
+    <br>
+
+
+    <div class="getTests">
+        <h1>Enter date for tests </h1>
+        <form method="post" action="../includes/lab_getTests.php" name="labtests">
+            <input type="date" name="date"  placeholder="Desired date" id="date" required />
+            <input type="submit" name="labtests" value="Search">
+        </form>
+    </div>
+
+
+    <table class="desiredResults" id="desiredResults">
+        <tr>
+            <th>UID</th>
+            <th>Serial #</th>
+            <th>Test Date</th>
+            <th>Result</th>
+            <th>is_signed</th>
+        <tr>
+        <tr>
+            <td><?php $row[0];?></td>
+            <td>B</td>
+            <td>C</td>
+            <td>D</td>
+            <td>E</td>
+        <tr>
+
+    </table>
+
+
+ 
+
+</body>
+</html>
