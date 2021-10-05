bd16d43a2a584a8f9d19a80eeb93b3937688aa55
Dustin1100 <Dustin1100@gmail.com>
2021-10-05 12:20:30 -0400

    Lab dashboard done

--- forms/lab/lab_dashboard.php
+++ forms/lab/lab_dashboard.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>lab dashboard</title>
+    <title>Lab user dashboard</title>
     <link href="../../css/dashboard.css" rel="stylesheet" type="text/css">
 </head>
 <body>
@@ -20,7 +20,7 @@
 
     <div class="getTests">
         <h1>Enter date for tests </h1>
-        <form method="post" action="../../includes/lab_getTests.php" name="labtests">
+        <form method="post" action="" name="labtests">
             <input type="date" name="date"  placeholder="Desired date" id="date" required />
             <input type="submit" name="labtests" value="Search">
         </form>
@@ -34,19 +34,68 @@
             <th>Test Date</th>
             <th>Result</th>
             <th>is_signed</th>
+            <th>TID</th>
         <tr>
-        <tr>
-            <td>A</td>
-            <td>B</td>
-            <td>C</td>
-            <td>D</td>
-            <td>E</td>
-        <tr>
+
+            <?php
+                include('../../includes/connect.php'); 
+	
+                if (isset($_POST['labtests'])) {
+                    $date = $_POST['date'];
+            
+                    $query_date = "SELECT * FROM `test_sample` WHERE `test_date` = '$date'"; //returns rows that have the desired date
+                    $result = $connect->query($query_date); //saves resultng data
+                    
+                    if($result->num_rows > 0) {
+                        //loop throught rows
+                        while(($row = $result->fetch_row())!==null) {
+                            //print out rows
+                            echo '<tr>';
+                            echo "<td>{$row[1]}</td>";
+                            echo "<td>{$row[2]}</td>";
+                            echo "<td>{$row[3]}</td>";
+                            echo "<td>{$row[4]}</td>";
+                            echo "<td>{$row[5]}</td>";
+                            echo "<td>TID = {$row[0]}</td>";
+                            echo '</tr>';
+                            echo '<tr>
+                            <td><hr></td><td><hr></td>
+                            <td><hr></td><td><hr></td>
+                            <td><hr></td><td><hr></td>
+                            </tr>';
+                        }
+                        //print out the form
+                        echo '<tr><td><br></td></tr>';
+                        echo '<tr>';
+                        echo '<td>Update Test: </td>';
+                        echo '<td>(All fields are required)</td>';
+                        echo '<form action="lab_dashboard.php" method="post">';
+                        echo '<td><input type="number" name="result" placeholder="Enter Result" id="result" pattern="[0-1]"/></td>';
+                        echo '<td><input type="number" name="tid" placeholder="Enter TID" id="tid" pattern="[0-1]"/></td>';
+                        echo '<td><input type="submit" name="submit" value="submit"></td>';
+                        echo '</form>';
+                        echo '</tr>';
+                    } else {
+                        echo '<tr><td>There were no tests on that date</td></tr>';
+                        
+                    }
+                }
+                if (isset($_POST['submit'])) { //checks if the submit button was pressed
+                    $result = $_POST['result']; //saves variables from the user's input
+                    $tid = $_POST['tid'];
+                    
+                    $update_result = "UPDATE `test_sample` SET `result` = $result WHERE `test_sample`.`TID` = $tid;"; //prepares sql statement to check if username already exists
+                    $result = $connect->query($update_result); //runs $select_user as a query and stores the result in $result
+
+                    if($connect->query($update_result) == TRUE) { //evan's query function, up for discussion on which to use
+                        echo "Result has been updated";
+                        header('Refresh: 1;URL=lab_dashboard.php');
+                    } else 
+                        echo "insertion failed for some reason. try again.";
+                    $connect->close();
+                }
+        ?>
 
     </table>
-
-
- 
-
 </body>
 </html>
