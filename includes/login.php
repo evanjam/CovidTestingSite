e04388dbb345dc4fc43000491fb798e987b8528b
Dustin1100 <Dustin1100@gmail.com>
2021-10-24 20:10:31 -0400

    Login now directs to correct dashboard
    Added session variables and redirects login to the correct dashboard

--- includes/login.php
+++ includes/login.php
@@ -3,6 +3,9 @@
 //https://stackoverflow.com/questions/46819734/how-to-check-username-and-password-matches-the-database-values
 //https://stackoverflow.com/questions/26536293/php-password-hash-password-verify
 
+	//Start session
+	session_start();
+
     include('connect.php'); 
 	
     if (isset($_POST['login'])) {
@@ -12,10 +15,29 @@
 		$result = $connect->query($get_user);
 		
 		if($result->num_rows > 0) {
-			$get_password = "SELECT password FROM user_profile WHERE username = '$username'";
 			$row = $result->fetch_array(MYSQLI_ASSOC);
+			$permission = $row['permission'];
 		if(password_verify($password, $row['password'])) {
+				$_SESSION['username'] = $username;
+				$_SESSION['permission'] = $permission;
 				echo "credentials match, username and password has been successfully verified!";
+				echo $_SESSION['permission'];
+				if($_SESSION['permission'] == 0){
+					header('Refresh: 1;URL=../forms/patient/patient_dashboard.php');
+				}
+				else if($_SESSION['permission'] == 1){
+					header('Refresh: 1;URL=../forms/employee/employee_dashboard.php');
+				}
+				else if($_SESSION['permission'] == 2){
+					header('Refresh: 1;URL=../forms/lab/lab_dashboard.php');
+				}
+				else if($_SESSION['permission'] == 3){
+					header('Refresh: 1;URL=../forms/doctor/doctor_dashboard.php');
+				}
+				else if($_SESSION['permission'] == 4){
+					header('Refresh: 1;URL=../forms/admin/admin_dashboard.php');
+				}
+
 			} else {
 				echo "credentials do not match what is stored in the db, try again";
 				header('Refresh: 1;URL=../forms/login.php');
