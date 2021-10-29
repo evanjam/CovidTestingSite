d002c24cd83a72f50fba32a50e59ea87a25539ad
Dustin1100 <Dustin1100@gmail.com>
2021-10-29 11:51:09 -0400

    Admin register error handling

--- forms/admin/admin_register.php
+++ forms/admin/admin_register.php
@@ -14,35 +14,52 @@
 </head>
 <body>
 <?php
-    //Check to see if the users permission level is correct for this page
-    if($_SESSION['permission'] == 4){
-        echo'<div class="header">
-            <h1>Admin/User Registration Form</h1>
-        
-        </div>
+    //This allows the error to be caught. It gets missed without line 19-28
+    set_error_handler('exceptions_error_handler');
 
-        <div><a href="admin_dashboard.php">Home</a></div>
-        <br>
-
-        <div class="employee_register">
-            <h1>User Registration Form</h1>
-            Please fill out the following form and press Register to register a new User. Enter permission level as number 0-4.
-            <img src="../../img/employee/nurse_resize.jpg" alt="Nurse">
-            <form method="post" action="../../includes/admin_register.php" name="register">
-                <input type="text" name="username" placeholder="username" required>
-                <input type="password" name="password" placeholder="password" required>
-                <input type="text" name="fname" placeholder="first name" required>
-                <input type="text" name="lname" placeholder="last name" required>
-                <input type="text" name="ssn" placeholder="ssn" pattern="[0-9]+" required>
-                <input type="date" name="date"  placeholder="Desired date (year-month-day)" id="date" required />
-                <input type="number" name="permission" placeholder="permission" id="permission" pattern="[0-4]" required />
-                <input type="submit" name="register" value="Register">
-            </form>
-        </div>';
+    function exceptions_error_handler($severity, $message, $filename, $lineno) {
+    if (error_reporting() == 0) {
+        return;
     }
-    else{
-        //Return message if access level is incorrect.
-        echo '<h1>This page is not reachable with your level of access.</h1>';
+    if (error_reporting() & $severity) {
+        throw new ErrorException($message, 0, $severity, $filename, $lineno);
+    }
+    }
+
+
+    //Check to see if the users permission level is correct for this page
+    try{
+        if($_SESSION['permission'] == 4){
+            echo'<div class="header">
+                <h1>Admin/User Registration Form</h1>
+            
+            </div>
+
+            <div><a href="admin_dashboard.php">Home</a></div>
+            <br>
+
+            <div class="employee_register">
+                <h1>User Registration Form</h1>
+                Please fill out the following form and press Register to register a new User. Enter permission level as number 0-4.
+                <img src="../../img/employee/nurse_resize.jpg" alt="Nurse">
+                <form method="post" action="../../includes/admin_register.php" name="register">
+                    <input type="text" name="username" placeholder="username" required>
+                    <input type="password" name="password" placeholder="password" required>
+                    <input type="text" name="fname" placeholder="first name" required>
+                    <input type="text" name="lname" placeholder="last name" required>
+                    <input type="text" name="ssn" placeholder="ssn" pattern="[0-9]+" required>
+                    <input type="date" name="date"  placeholder="Desired date (year-month-day)" id="date" required />
+                    <input type="number" name="permission" placeholder="permission" id="permission" pattern="[0-4]" required />
+                    <input type="submit" name="register" value="Register">
+                </form>
+            </div>';
+        }
+        else{
+            //Return message if access level is incorrect.
+            echo '<h1>This page is not reachable with your level of access.</h1>';
+        }
+    }catch(Exception $e){
+        echo'<h1>This page is unavailable.</h1>';
     }
 ?>
 
