4855ccdd15ece261ddd8cfe43dd22d0ec3aea092
Dustin1100 <Dustin1100@gmail.com>
2021-10-29 12:07:57 -0400

    Patient dashboard security
    Added a check to see if the permission variable exists on patient dashboard so that you can't go to the page without logging in. Then added error handling incase the permission variable does not exist

--- forms/patient/patient_dashboard.php
+++ forms/patient/patient_dashboard.php
@@ -13,6 +13,21 @@
 </head>
 <body>
     <?php
+    //This allows the error to be caught. It gets missed without line 19-28
+    set_error_handler('exceptions_error_handler');
+
+    function exceptions_error_handler($severity, $message, $filename, $lineno) {
+    if (error_reporting() == 0) {
+        return;
+    }
+    if (error_reporting() & $severity) {
+        throw new ErrorException($message, 0, $severity, $filename, $lineno);
+    }
+    }
+
+    try{
+    if($_SESSION['permission'] > -1 && $_SESSION['permission'] < 5){
+
     echo'
     <div class="header">
         <h1>Patient dashboard</h1>
@@ -79,6 +94,13 @@
                         echo "<td>There are no test results available</td>";  
                         echo '</tr>';                   
                 }
+            }else{
+                echo '<h1>This page is not reachable with your level of access.</h1>';
+            }
+            }catch(Exception $e){
+                echo'<h1>This page is unavailable.</h1>';
+                header('Refresh: 1;URL=../../index.php');
+            }
         ?>
 
     </table>
