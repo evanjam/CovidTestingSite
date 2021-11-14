04f251525b871b625db4fa9c28733de84984ad28
Dustin1100 <Dustin1100@gmail.com>
2021-11-14 11:07:07 -0500

    Login class created and fixed display of Forgot/create pw during processing
    Created a class for the lengthy login displays. Removed ability to click forgot pw ad create account during loading. Also disabled login button during loading.

---
+++ classes/login.classes.php
@@ -0,0 +1,101 @@
+<?php
+//Contains the functions for login.php
+class Login{
+
+    //Successful login function
+    public function success_Login(){
+        echo '
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>test login/register page</title>
+                <link href="../css/login.css" rel="stylesheet" type="text/css">
+            </head>
+            <body>
+
+                <div class="login">
+                    <h1>login</h1>
+                    <form method="post" action="includes/login.php" name="login">
+                        <input type="text" name="username" pattern="[a-zA-Z0-9]+" placeholder="login" id="username" required />
+                        <input type="password" name="password" placeholder="password" required>
+                        <input type="submit" name="login" value="login" disabled>
+                    </form>
+                    <hr>
+                    <div>Credentials match, Logging in...</div>
+                    <div>(Log created)</div>
+                    <hr>
+                </div>
+
+            </body>
+            </html>';
+    }
+
+    //Wrong credential function
+    public function wrong_Credentials(){
+        echo '
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>test login/register page</title>
+            <link href="../css/login.css" rel="stylesheet" type="text/css">
+        </head>
+        <body>
+
+            <div class="login">
+                <h1>login</h1>
+                <form method="post" action="includes/login.php" name="login">
+                    <input type="text" name="username" pattern="[a-zA-Z0-9]+" placeholder="login" id="username" required />
+                    <input type="password" name="password" placeholder="password" required>
+                    <input type="submit" name="login" value="login" disabled>
+                </form>
+                <hr>
+                <div>Credentials do not match, Try again</div> <!--password verify fails -->
+                <div>(Log created)</div>
+                <hr>
+            </div>
+
+        </body>
+        </html>';
+    }
+
+    //User does not exist function
+    public function user_Not_Exist(){
+        echo '
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>test login/register page</title>
+                <link href="../css/login.css" rel="stylesheet" type="text/css">
+            </head>
+            <body>
+
+                <div class="login">
+                    <h1>login</h1>
+                    <form method="post" action="includes/login.php" name="login">
+                        <input type="text" name="username" pattern="[a-zA-Z0-9]+" placeholder="login" id="username" required />
+                        <input type="password" name="password" placeholder="password" required>
+                        <input type="submit" name="login" value="login" disabled>
+                    </form>
+                    <hr>
+                    <div>Credentials do not match, Try again</div> <!--user does not exist-->
+                    <div>(No log created)</div>
+                    <hr>
+                </div>
+
+            </body>
+            </html>';
+    }
+}
+
+
+
+?>
--- includes/login.php
+++ includes/login.php
@@ -6,7 +6,7 @@
 	//Start session
 	session_start();
 
-    include('connect.php'); 
+    include('connect.php');
 	
     if (isset($_POST['login'])) {
         $username = $_POST['username'];
@@ -32,35 +32,9 @@
 				$connect->query($insert_log); //executing sql statement to insert into login_log table
 				
 				//re-printing the login page front end but with an added div at the bottom indicating login status
-				echo '
-				<!DOCTYPE html>
-				<html lang="en">
-				<head>
-					<meta charset="UTF-8">
-					<meta http-equiv="X-UA-Compatible" content="IE=edge">
-					<meta name="viewport" content="width=device-width, initial-scale=1.0">
-					<title>test login/register page</title>
-					<link href="../css/login.css" rel="stylesheet" type="text/css">
-				</head>
-				<body>
-
-					<div class="login">
-						<h1>login</h1>
-						<form method="post" action="includes/login.php" name="login">
-							<input type="text" name="username" pattern="[a-zA-Z0-9]+" placeholder="login" id="username" required />
-							<input type="password" name="password" placeholder="password" required>
-							<input type="submit" name="login" value="login">
-						</form>
-						<a href="forms/pass_reset.php">Forgot password?</a><br>
-						<a href="forms/patient_register.php">Create New Account</a>
-						<hr>
-						<div>Credentials match, Logging in...</div>
-						<div>(Log created)</div>
-						<hr>
-					</div>
-
-				</body>
-				</html>';
+				include('../classes/login.classes.php');
+				$obj = new Login();
+				$obj->success_Login();
 
 				//Check the user permission and redirect to appropriate dashboard based on permission level
 				if($_SESSION['permission'] == 0){
@@ -84,70 +58,19 @@
 				$insert_log = "INSERT INTO login_log (UID, login_date, is_successful) VALUES ('$UID', '$login_date', '0')"; //preparing sql statement 
 				$connect->query($insert_log); //executing sql statement to insert into login_log table
 				//re-printing the login page front end but with an added div at the bottom indicating login status
-				echo '
-				<!DOCTYPE html>
-				<html lang="en">
-				<head>
-					<meta charset="UTF-8">
-					<meta http-equiv="X-UA-Compatible" content="IE=edge">
-					<meta name="viewport" content="width=device-width, initial-scale=1.0">
-					<title>test login/register page</title>
-					<link href="../css/login.css" rel="stylesheet" type="text/css">
-				</head>
-				<body>
+				include('../classes/login.classes.php');
+				$obj = new Login();
+				$obj->wrong_Credentials();
 
-					<div class="login">
-						<h1>login</h1>
-						<form method="post" action="includes/login.php" name="login">
-							<input type="text" name="username" pattern="[a-zA-Z0-9]+" placeholder="login" id="username" required />
-							<input type="password" name="password" placeholder="password" required>
-							<input type="submit" name="login" value="login">
-						</form>
-						<a href="forms/pass_reset.php">Forgot password?</a><br>
-						<a href="forms/register.php">Create New Account</a>
-						<hr>
-						<div>Credentials do not match you dum idiot, Try again</div> <!--password verify fails -->
-						<div>(Log created)</div>
-						<hr>
-					</div>
-
-				</body>
-				</html>';
 				header('Refresh: 2;URL=../index.php');
 			}
 		} else {
 			//if the username does not exist in the database then do not insert a log entry and print message denying access, refresh page
 			
 			//re-printing the login page front end but with an added div at the bottom indicating login status
-			echo '
-				<!DOCTYPE html>
-				<html lang="en">
-				<head>
-					<meta charset="UTF-8">
-					<meta http-equiv="X-UA-Compatible" content="IE=edge">
-					<meta name="viewport" content="width=device-width, initial-scale=1.0">
-					<title>test login/register page</title>
-					<link href="../css/login.css" rel="stylesheet" type="text/css">
-				</head>
-				<body>
-
-					<div class="login">
-						<h1>login</h1>
-						<form method="post" action="includes/login.php" name="login">
-							<input type="text" name="username" pattern="[a-zA-Z0-9]+" placeholder="login" id="username" required />
-							<input type="password" name="password" placeholder="password" required>
-							<input type="submit" name="login" value="login">
-						</form>
-						<a href="forms/pass_reset.php">Forgot password?</a><br>
-						<a href="forms/register.php">Create New Account</a>
-						<hr>
-						<div>Credentials do not match, Try again</div> <!--user does not exist-->
-						<div>(No log created)</div>
-						<hr>
-					</div>
-
-				</body>
-				</html>';
+			include('../classes/login.classes.php');
+			$obj = new Login();
+			$obj->user_Not_Exist();
 			header('Refresh: 2;URL=../index.php');
 		}
 
