b0b383973e25bab57ac53f413fd860be46f162ca
evanjam <deadhead9420@gmail.com>
2021-11-14 12:41:33 -0500

    database update
    -split user profile into 2 tables, one for user_profile and one for user_info.
    -breaks most of the forms
    -please use this while you are making changes to affected forms

--- sql/sc19_alpha_update_11-14.sql
+++ sql/sc19_alpha_update_11-14.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 11, 2021 at 02:04 AM
+-- Generation Time: Nov 14, 2021 at 06:35 PM
 -- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.10
+-- PHP Version: 7.3.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,18 @@ CREATE TABLE `login_log` (
   `login_date` date NOT NULL,
   `is_successful` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login_log`
+--
+
+INSERT INTO `login_log` (`LLID`, `UID`, `login_date`, `is_successful`) VALUES
+(15, 1, '2021-11-14', 1),
+(16, 2, '2021-11-14', 0),
+(17, 2, '2021-11-14', 1),
+(18, 3, '2021-11-14', 1),
+(19, 4, '2021-11-14', 1),
+(20, 4, '2021-11-14', 1);
 
 -- --------------------------------------------------------
 
@@ -94,7 +106,7 @@ CREATE TABLE `user_profile` (
 
 INSERT INTO `user_profile` (`UID`, `username`, `password`, `fname`, `lname`, `dob`, `ssn`, `permission`) VALUES
 (1, 'a', '$2y$10$kzhwCTKalXGfti0XYx5VkuWeZj2QDkLC4JV1Tm0fI72EckLFou94e', 'a', 'a', '2021-10-01', 111111111, 4),
-(2, 'd', '$2y$10$Htlgs/jtjWo8uevGJQAYKumFxNJg1AxKMXCbXQ6lvDumirxiNYZnq', 'd', 'd', '2021-10-01', 222222222, 3),
+(2, 'd', '$2y$10$XUlmaC.rb3LIrMCEXe1a5.SdF.O3JQM.YLka4NgJV5krUypVdrV66', 'd', 'd', '2021-10-01', 222222222, 3),
 (3, 'l', '$2y$10$Iex7HvEUoylESDSzXfJXNupakumMUGFz8xQcGNsPtVoATLK3Uy382', 'l', 'l', '2021-10-01', 333333333, 2),
 (4, 'e', '$2y$10$r8dD1nNNeNhGmDcJZkIrMumzeLfCIjN/FHF5QOGFWRpUgFOUwMBIS', 'e', 'e', '2021-10-01', 444444444, 1),
 (5, 'p', '$2y$10$G31XsG/5TeqZBHNW/9DCL.I.wr8eWr2IHH4nYFDyemHcTSz0HKVMq', 'p', 'p', '2021-10-01', 555555555, 0),
@@ -110,7 +122,8 @@ INSERT INTO `user_profile` (`UID`, `username`, `password`, `fname`, `lname`, `do
 -- Indexes for table `login_log`
 --
 ALTER TABLE `login_log`
-  ADD PRIMARY KEY (`LLID`);
+  ADD PRIMARY KEY (`LLID`),
+  ADD KEY `UID` (`UID`);
 
 --
 -- Indexes for table `test_sample`
@@ -140,7 +153,7 @@ ALTER TABLE `user_profile`
 -- AUTO_INCREMENT for table `login_log`
 --
 ALTER TABLE `login_log`
-  MODIFY `LLID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `LLID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `test_sample`
@@ -163,6 +176,12 @@ ALTER TABLE `user_profile`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `login_log`
+--
+ALTER TABLE `login_log`
+  ADD CONSTRAINT `login_log_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `user_profile` (`UID`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Constraints for table `test_sample`
