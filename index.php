c4201ac3af5723c54f90c27faef09f09e36ff465
evanjam <deadhead9420@gmail.com>
2021-11-21 20:05:30 -0500

    change title of login page

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test login/register page</title>
+    <title>CTS Testing Services Login</title>
     <link href="css/login.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 
     <div class="login">
-        <h1>login</h1>
+        <h1>CTS Testing</h1>
         <form method="post" action="includes/login.php" name="login">
             <input type="text" name="username" pattern="[a-zA-Z0-9]+" placeholder="login" id="username" required />
             <input type="password" name="password" placeholder="password" required>
