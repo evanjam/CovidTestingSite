3b2631e311568a50a2bbe920bd2d6c2620a90a00
Dustin1100 <Dustin1100@gmail.com>
2021-11-28 21:24:36 -0500

    Admin submit test style

--- forms/admin/admin_submit_test.php
+++ forms/admin/admin_submit_test.php
@@ -21,7 +21,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Submit New Test</title>
-    <link href="../../css/dashboard.css" rel="stylesheet" type="text/css">
+    <link href="../../css/employee_Submit.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 
@@ -51,13 +51,15 @@
             <div><a href="admin_dashboard.php">Home</a></div>
             
 
-            <div class="employee_register">
-                <h1>Submit New Test</h1>
-                Please enter the patients username and the serial # on the test vial and press Submit
+            <div class="form">
+                <h1 class="Heading">Submit New Test</h1>
+                <p>Please enter the patients username and the serial # on the test vial and press Submit</p>
                 <img src="../../img/employee/cotton_swab_resize.jpg" alt="Cotton Swab">
+                <br></br>
                 <form method="post" action="../../includes/submit_test.php" name="submit_test">
                     <input type="text" name="username" placeholder="username" required>
                     <input type="text" name="serial" placeholder="serial #" pattern="[0-9]+" required>
+                    <br></br>
                     <input type="submit" name="submit_test" value="Submit">
                 </form>
                 
@@ -73,6 +75,10 @@
     }
 ?>
 	
-
+    <footer>
+        <div>
+            <p>Covid Testing Site 2021</p>
+        </div>
+    </footer>
 </body>
 </html>
