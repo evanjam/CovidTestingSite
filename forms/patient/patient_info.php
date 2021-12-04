9316cb58a61747c458d16f917a49bad0a3026dee
Sunnyjava101 <sunsinghmultani1@gmail.com>
2021-12-04 10:49:36 -0500

    Commenting

--- forms/patient/patient_info.php
+++ forms/patient/patient_info.php
@@ -1,12 +1,14 @@
 <?php
     session_start();
-	
+	//user inactivity logout, create $t to time() for unix Timestamp
 	$t=time();
-	if (isset($_SESSION['logged']) && ($t - $_SESSION['logged'] > 900)) 
+	//if user is logged and user hasn't interacted with the page for 15 mins, destroy session, redirect
+	if (isset($_SESSION['logged']) && ($t - $_SESSION['logged'] > 900))
 	{
     session_destroy();
 	header('Refresh: 0.01;URL=../../sessioninactivitytimeout.php');
 	}
+	//if not User refreshes or clicks another link, start time() again
 	else
 	{
 		$_SESSION['logged'] = time();
@@ -25,6 +27,7 @@
 <body>
 	
 	<?php
+	//connect to the DB and set the header and make the Home button
 	include('../../includes/connect.php'); 
 	
 		echo'
@@ -36,15 +39,19 @@
 		<li><a href="patient_dashboard.php">Home</a></li>
 		</ul>
 		<br>';
-	
+		//make a session variable called UID
 		$UID = $_SESSION['UID'];
-		$get_user = "SELECT * FROM `user_profile` WHERE `UID` = '$UID'";;
+		//Create get_user and SELECT in user_profile table where UID of user matches UID
+		$get_user = "SELECT * FROM `user_profile` WHERE `UID` = '$UID'";
+		//runs $get_user as a query and stores the result in $result
 		$result = $connect->query($get_user);
-		
+		//if $result->num_rows > 0 returns true, then there exists rows where the UID=$UID 
 		if($result->num_rows > 0) 
 		{
+			//While the row we fetched is not null, perform the if statements
 			while(($row = $result->fetch_row())!==null)
 			{
+				//Iterate through the row to check if it null, if not print whats in the index
 				if($row[3] != null)
 				{
 					print "<p><b>First Name: $row[3]</b></p>";
@@ -60,6 +67,7 @@
 				}
 				if($row[6] != null)
 				{
+					//Create $part to get a substring of s string, in this case SSN to hide part of it
 					$part = substr($row[6], -4);
 					print "<p><b>Social Security: xxx-xx-$part</b></p>";
 				}
@@ -78,7 +86,6 @@
 			}
 		}
 	?>
-
   <footer>
         <div>
             <p>Covid Testing Site 2021</p>
