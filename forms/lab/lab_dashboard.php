00d2f0066efab860e1070bd23f5077f9855d8b7f
Dustin1100 <Dustin1100@gmail.com>
2021-12-06 16:15:00 -0500

    Lab Dashboard cleaned up
    Fixed the commenting, formatting of lines, excessive echo statements.

--- forms/lab/lab_dashboard.php
+++ forms/lab/lab_dashboard.php
@@ -4,8 +4,8 @@
 	$t=time();
 	if (isset($_SESSION['logged']) && ($t - $_SESSION['logged'] > 900)) 
 	{
-    session_destroy();
-	header('Refresh: 0.01;URL=../../sessioninactivitytimeout.php');
+        session_destroy();
+        header('Refresh: 0.01;URL=../../sessioninactivitytimeout.php');
 	}
 	else
 	{
@@ -14,175 +14,198 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab user dashboard</title>
-    <link href="../../css/dashboards.css" rel="stylesheet" type="text/css">
-</head>
-<body>
-    <?php
-    //This allows the error to be caught. It gets missed without line 19-28
-    set_error_handler('exceptions_error_handler');
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Lab user dashboard</title>
+        <link href="../../css/dashboards.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <?php
+        //This allows the error to be caught.
+        set_error_handler('exceptions_error_handler');
 
-    function exceptions_error_handler($severity, $message, $filename, $lineno) {
-    if (error_reporting() == 0) {
-        return;
-    }
-    if (error_reporting() & $severity) {
-        throw new ErrorException($message, 0, $severity, $filename, $lineno);
-    }
-    }
-    
-    try{
-    if($_SESSION['permission'] == 2){ 
-    echo'<div class="header">';
-        echo'<h1>Lab user dashboard</h1>';
-    
-    echo'</div>';
-	
-    echo'<ul>
-    <li><a href="lab_dashboard.php">Home</a></li>
-    <br>
-    <li><a href="../patient/patient_dashboard.php">View Personal Test Results</a></li>
-    <br>
-    <li><a href="../../index.php">Log Out</a></li>
-    </ul>';
-
-
-    echo'<div class="getTests">';
-        echo'<h1 class="labHeading">Enter date for tests </h1>';
-        echo'<hr>';
-        echo'<form method="post" action="" name="labtests" class="labDate">';
-            echo'<input type="date" name="date"  placeholder="Desired date" id="date" required />';
-            echo'<input type="submit" name="labtests" value="Search">';
-        echo'</form>';
-    echo'</div>';
-
-
-    echo'<table class="desiredResults" id="desiredResults">';
-        echo'<tr>';
-            echo'<th>UID</th>
-            <th>Serial #</th>
-            <th>Test Date</th>
-            <th>Result</th>
-            <th>is_signed</th>
-            <th>TID</th>';
-        echo'<tr>';
-
-                include('../../includes/connect.php'); 
-	
-                if (isset($_POST['labtests'])) {
-                    $date = $_POST['date'];
+        function exceptions_error_handler($severity, $message, $filename, $lineno) {
+            if (error_reporting() == 0) {
+                return;
+            }
+            if (error_reporting() & $severity) {
+                throw new ErrorException($message, 0, $severity, $filename, $lineno);
+            }
+        }
+        
+        try{
+            //Check to make sure that the user has the admin permission level of 2.
+            if($_SESSION['permission'] == 2){ 
             
-                    $query_date = "SELECT * FROM `test_sample` WHERE `test_date` = '$date'"; //returns rows that have the desired date
-                    $result = $connect->query($query_date); //saves resultng data
-                    
-                    if($result->num_rows > 0) {
-                        //loop throught rows
-                        while(($row = $result->fetch_row())!==null) {
-                            //print out rows
-                            echo '<tr>';
-                            echo "<td>{$row[1]}</td>";
-                            echo "<td>{$row[2]}</td>";
-                            echo "<td>{$row[3]}</td>";
-                            if($row[4] == 0){
-                                echo "<td>No Result</td>";
-                            }else if($row[4] == 1){
-                                echo "<td>Negative</td>";
-                            }else if($row[4] == 2){
-                                echo "<td>Positive</td>";
-                            }
+            //Print out the header, navigation bar, and input for date and submit.
+            echo
+            '<div class="header">
+                <h1>Lab user dashboard</h1>
+            </div>
+            <ul>
+            <li><a href="lab_dashboard.php">Home</a></li>
+            <br>
+            <li><a href="../patient/patient_dashboard.php">View Personal Test Results</a></li>
+            <br>
+            <li><a href="../../index.php">Log Out</a></li>
+            </ul>
+            <div class="getTests">
+                <h1 class="labHeading">Enter date for tests </h1>
+                <hr>
+                <form method="post" action="" name="labtests" class="labDate">
+                    <input class="inputColor" type="date" name="date"  placeholder="Desired date" id="date" required />
+                    <input class="inputColor" type="submit" name="labtests" value="Search">
+                </form>
+            </div>';
 
-                            if($row[5] == 0){
-                                echo "<td>Not Signed</td>";
-                            }else if($row[5] == 1){
-                                echo "<td>Signed</td>";
+            //Print out the top row of the table with the labels for each row.
+            echo
+            '<table class="desiredResults" id="desiredResults">
+                <tr>
+                    <th>UID</th>
+                    <th>Serial #</th>
+                    <th>Test Date</th>
+                    <th>Result</th>
+                    <th>is_signed</th>
+                    <th>TID</th>
+                <tr>';
+                        //Create connection to the DB
+                        include('../../includes/connect.php'); 
+
+                        //Handle when the submit button is clicked
+                        if (isset($_POST['labtests'])) {
+
+                            //Set the date to the current date
+                            $date = $_POST['date'];
+                    
+                            //Select all test samples for the current date
+                            $query_date = "SELECT * FROM `test_sample` WHERE `test_date` = '$date'";
+
+                            //Saves the data that was returned from the select query
+                            $result = $connect->query($query_date);
+                            
+                            //Check to see if there were results
+                            if($result->num_rows > 0) {
+
+                                //Loop through the rows
+                                while(($row = $result->fetch_row())!==null) {
+
+                                    //Print out the rows
+                                    echo '<tr>';
+                                    echo"<td>{$row[1]}</td>";
+                                    echo"<td>{$row[2]}</td>";
+                                    echo"<td>{$row[3]}</td>";
+
+                                    //Check the value of the test result
+                                    //0 == Null. 1 == Negative. 2 == Positive.
+                                    if($row[4] == 0){
+                                        echo "<td>No Result</td>";
+                                    }else if($row[4] == 1){
+                                        echo "<td>Negative</td>";
+                                    }else if($row[4] == 2){
+                                        echo "<td>Positive</td>";
+                                    }
+
+                                    //Check the value to determine if signed
+                                    //0 == Not signed. 1 == Signed.
+                                    if($row[5] == 0){
+                                        echo "<td>Not Signed</td>";
+                                    }else if($row[5] == 1){
+                                        echo "<td>Signed</td>";
+                                    }
+                                    echo "<td>TID = {$row[0]}</td>";
+                                    echo '</tr>';
+                                    echo '<tr>
+                                    <td><hr></td><td><hr></td>
+                                    <td><hr></td><td><hr></td>
+                                    <td><hr></td><td><hr></td>
+                                    </tr>';
+                                }
+                                //Print out the title rows for the submit form
+                                echo '
+                                <tr><td><br></br></td></tr>
+                                <tr><td>
+                                </td></tr>
+                                <tr>
+                                <td>Update Test: </td>
+                                <td>(All fields are required)</td>
+                                <td>Result</td>
+                                <td>Corresponding TID</td>
+                                <td></td>
+                                </tr>';
+                                //Print out a row with horizontal lines
+                                echo '
+                                <tr>
+                                <td></td>
+                                </tr><tr>
+                                <td></td>
+                                <td></td>
+                                <td><hr></td>
+                                <td><hr></td>
+                                <td></td>
+                                </tr>';
+
+                                //Print out the result input and the submit button at the bottom of the table
+                                echo '<tr>
+                                <td></td>
+                                </tr>
+                                <tr>
+                                <td></td>
+                                <td></td>
+                                <form action="lab_dashboard.php" method="post">
+                                <td><input type="radio" id="noResult" name="result" value="0">
+                                <label for="noResult">No Result</label><br>
+                                <input type="radio" id="positive" name="result" value="2">
+                                <label for="positive">Positive</label><br>
+                                <input type="radio" id="negative" name="result" value="1">
+                                <label for="negative">Negative</label></td>';
+                                //<td><input type="number" name="result" placeholder="Enter Result" id="result" pattern="[0-1]"/></td>
+                                echo'<td><input class="inputColor" type="number" name="tid" placeholder="Enter TID" id="tid" pattern="[0-1]"/></td>
+                                <td><input class="inputColor" type="submit" name="submit" value="submit"></td>
+                                </form>
+                                </tr>';
+                            } else {
+                                echo '<tr><td>There were no tests on that date</td></tr>';
+                                
                             }
-                            echo "<td>TID = {$row[0]}</td>";
-                            echo '</tr>';
-                            echo '<tr>
-                            <td><hr></td><td><hr></td>
-                            <td><hr></td><td><hr></td>
-                            <td><hr></td><td><hr></td>
-                            </tr>';
                         }
-                        //print out the form
-                        echo '
-                        <tr><td><br></br></td></tr>
-                        <tr><td>
-                        </td></tr>
-                        <tr>
-                        <td>Update Test: </td>
-                        <td>(All fields are required)</td>
-                        <td>Result</td>
-                        <td>Corresponding TID</td>
-                        <td></td>
-                        </tr>';
+                        //Checks if the submit button was pressed
+                        if (isset($_POST['submit'])) {
+                            //Saves variables from the user's input
+                            $result = $_POST['result']; 
+                            $tid = $_POST['tid'];
 
-                        echo '
-                        <tr><td>
-                        </td></tr>
-                        <tr>
-                        <td></td>
-                        <td></td>
-                        <td><hr></td>
-                        <td><hr></td>
-                        <td></td>
-                        </tr>';
+                            //Sql statement to update the information in the Database.
+                            $update_result = "UPDATE `test_sample` SET `result` = $result WHERE `test_sample`.`TID` = $tid;";
+                            //Stores the result of the query in $result
+                            $result = $connect->query($update_result); 
 
-                        echo '<tr><td>
-                        </td></tr>
-                        <tr>
-                        <td></td>
-                        <td></td>
-                        <form action="lab_dashboard.php" method="post">
-                        <td><input type="radio" id="noResult" name="result" value="0">
-                        <label for="noResult">No Result</label><br>
-                        <input type="radio" id="positive" name="result" value="2">
-                        <label for="positive">Positive</label><br>
-                        <input type="radio" id="negative" name="result" value="1">
-                        <label for="negative">Negative</label></td>';
-                        //<td><input type="number" name="result" placeholder="Enter Result" id="result" pattern="[0-1]"/></td>
-                        echo'<td><input type="number" name="tid" placeholder="Enter TID" id="tid" pattern="[0-1]"/></td>
-                        <td><input type="submit" name="submit" value="submit"></td>
-                        </form>
-                        </tr>';
-                    } else {
-                        echo '<tr><td>There were no tests on that date</td></tr>';
-                        
-                    }
-                }
-                if (isset($_POST['submit'])) { //checks if the submit button was pressed
-                    $result = $_POST['result']; //saves variables from the user's input
-                    $tid = $_POST['tid'];
-                    
-                    $update_result = "UPDATE `test_sample` SET `result` = $result WHERE `test_sample`.`TID` = $tid;"; //prepares sql statement to check if username already exists
-                    $result = $connect->query($update_result); //runs $select_user as a query and stores the result in $result
-
-                    if($connect->query($update_result) == TRUE) { //evan's query function, up for discussion on which to use
-                        echo "Result has been updated";
-                        header('Refresh: 1;URL=lab_dashboard.php');
-                    } else 
-                        echo "insertion failed for some reason. try again.";
-                    $connect->close();
-                }
-    }
-    else{
-        echo '<h1>This page is not reachable with your level of access.</h1>';
-    }
-}catch(Exception $e){
-    echo'<h1>This page is unavailable.</h1>';
-    header('Refresh: 1;URL=../../index.php');
-}
+                            //Check to see if the query was successful
+                            if($connect->query($update_result) == TRUE) {
+                                echo "Result has been updated";
+                                header('Refresh: 1;URL=lab_dashboard.php');
+                            } else 
+                                echo "insertion failed for some reason. try again.";
+                            $connect->close();
+                        }
+            }
+            else{
+                //If the permission level was no 2.
+                echo '<h1>This page is not reachable with your level of access.</h1>';
+            }
+            //If the session variable did not exist catch the error.
+        }catch(Exception $e){
+            echo'<h1>This page is unavailable.</h1>';
+            header('Refresh: 1;URL=../../index.php');
+        }
         ?>
-    </table>
-    <footer>
-        <div>
-            <p>Covid Testing Site 2021</p>
-        </div>
-    </footer>
-
-</body>
+        </table>
+        <footer>
+            <div>
+                <p>Covid Testing Site 2021</p>
+            </div>
+        </footer>
+    </body>
 </html>
