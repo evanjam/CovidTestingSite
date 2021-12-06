7695e0bdcc9e94563201ec30bb70ca8a0c4e294b
evanjam <deadhead9420@gmail.com>
2021-12-06 18:32:27 -0500

    evan items
    -comment and clean up code in email_verify and resend_email
    -changes to tables powerpoint
    -create forms_sample powerpoint (not done but at least its called sample)

--- includes/email_verify.php
+++ includes/email_verify.php
@@ -5,36 +5,50 @@
 //todo: implement email logging in a new table email_log
 
 //for testing use this url:
-//http://localhost/covidtestingsite/includes/email_verify.php?username=e&email=email@email.com
+//http://localhost/CovidTestingSite/includes/email_verify.php?username=cts.sendmail2021@gmail.com&email_token=8065d07da4a77621450aa84fee5656d9
 
+	//establishes connect to mysql database
+	include('connect.php');
+	
+	//pulls variables from the URL that is typed in, see example syntax on line 8
+	$username = $_GET['username'];
+	$email_token = $_GET['email_token'];
 
-		include('connect.php');
+	//prepares and executes sql query to select user from database where username matches the username in the URL
+	$select_user = "SELECT * FROM user_profile WHERE username = '$username'";
+	$result = $connect->query($select_user);
 		
-		$username = $_GET['username']; //pulls variable from the URL using ?username= in url
-		$email_token = $_GET['email_token']; //pulls variable from the URL using $email= in url
-
-		$select_user = "SELECT * FROM user_profile WHERE username = '$username'"; //prepares sql statement to check if username already exists
-		$result = $connect->query($select_user); //runs $select_user as a query and stores the result in $result
-		
-		if($result->num_rows > 0) { //if $result->num_rows > 0 returns true, then there exists rows where the username=$username 
-			$row = $result->fetch_array(MYSQLI_ASSOC); //explode information from the username table where $username = input from URL bar
+	//if num_rows is greater than 0, than there exists rows where the username equals username in URL. therefore user exists. 
+	if($result->num_rows > 0) {
+		//"explode" information from the returned row into an array with individually accessible fields that can be called via their field name
+		//$row contains the array
+		$row = $result->fetch_array(MYSQLI_ASSOC); 
 			
-			//compare username and email_token from URL with the information in database, created during registration
-			if($username == $row['username'] && $email_token == $row['email_token']) {
+		//compare username and email_token from URL with the information in database, created during registration
+		//if the username in the URL matches the username from the selected row
+		//AND the email token matches the email token in the selected row
+		if($username == $row['username'] && $email_token == $row['email_token']) {
+			
+			//print echo statement (can still be cleaned up to print a nicer looking message)
+			echo 'email successfully verified. please return to home page and sign in.';
 				
-				echo 'email successfully verified. please return to home page and sign in.';
+			//prepare and execute sql query to update the email_verify field from 0 to 1
+			$update_verification_status = "UPDATE `user_profile` SET `email_verified` = '1' WHERE `user_profile`.`username` = '$username'";
+			$connect->query($update_verification_status);
+			
+			//prepare and execute sql query to update the email_token variable back to a NULL status
+			//this prevents the same email verification link from being used again
+			$update_email_token = "UPDATE `user_profile` SET `email_token` = '' WHERE `user_profile`.`username` = '$username'";
+			$connect->query($update_email_token);
 				
-				$update_verification_status = "UPDATE `user_profile` SET `email_verified` = '1' WHERE `user_profile`.`username` = '$username'"; //set up query for next line
-				$connect->query($update_verification_status); //update email_verified from 0 to 1
-				$update_email_token = "UPDATE `user_profile` SET `email_token` = '' WHERE `user_profile`.`username` = '$username'"; //set up query for next line
-				$connect->query($update_email_token); //reset email_token back to a NULL value
-				
-				header('Refresh: 2;URL=../index.php');
-			}
-			else {
-				echo 'invalid token or the verification has already been processed. try again.';
-				header('Refresh: 2;URL=../index.php');
-			}
+			//redirect to login screen
+			header('Refresh: 2;URL=../index.php');
+
+
+		} else {
+			echo 'invalid token or the verification has already been processed. try again.';
+			header('Refresh: 2;URL=../index.php');
 		}
+	}
 
 ?>
--- includes/employee/resend_verify.php
+++ includes/employee/resend_verify.php
@@ -22,8 +22,9 @@
     <link href="../../css/dashboards.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <?php
-    //This allows the error to be caught. It gets missed without line 19-28
+
+<?php
+    //sets up error handling for Try-catch block on line 39
     set_error_handler('exceptions_error_handler');
 
     function exceptions_error_handler($severity, $message, $filename, $lineno) {
@@ -35,58 +36,61 @@
     }
     }
     
+	//try catch block
     try{
-    if($_SESSION['permission'] == 1 || $_SESSION['permission'] == 4){ 
-    echo'<div class="header">';
-        echo'<h1>Resend Verification Email</h1>';
-    
-    echo'</div>';
-	
-	if($_SESSION['permission'] == 1) {
-		echo'<ul>
-    <li><a href="../../forms/employee/employee_dashboard.php">Home</a>
-	<a href="../../forms/employee/employee_view_user.php">Back</a></li>
-    </ul>';
-	}
-	
-	if($_SESSION['permission'] == 4) {
-		echo'<ul>
-    <li><a href="../../forms/admin/admin_dashboard.php">Home</a>
-	<a href="../../forms/admin/admin_edit_user.php">Back</a></li>
-    </ul>';
-	}
+		
+		//print the html front end for employee (1) and admin (4) users ONLY
+		if($_SESSION['permission'] == 1 || $_SESSION['permission'] == 4){ 
+			echo'<div class="header">
+			<h1>Resend Verification Email</h1>
+			</div>';
+		
+			//if employee user is viewing page, make sure the home/back buttons are redirecting to employee dash
+			if($_SESSION['permission'] == 1) {
+				echo'<ul>
+			<li><a href="../../forms/employee/employee_dashboard.php">Home</a>
+			<a href="../../forms/employee/employee_view_user.php">Back</a></li>
+			</ul>';
+			}
+		
+			//if admin user is viewing page, make sure the home/back buttons are redirecting to admin dash
+			if($_SESSION['permission'] == 4) {
+				echo'<ul>
+			<li><a href="../../forms/admin/admin_dashboard.php">Home</a>
+			<a href="../../forms/admin/admin_edit_user.php">Back</a></li>
+			</ul>';
+			}
 
-
-    echo'<div class="getTests">';
-        echo'<h1 class="labHeading">Enter a username</h1>';
-        echo'<form method="post" action="" name="users">';
-            echo'<div class="labHeading"><input type="text" name="user"  placeholder="username" id="user" required/>
-            <input type="submit" name="users" value="Resend Verification Email"></div>
-    ';
-        echo'</form>';
-    echo'</div>';
-
-                include('../../includes/connect.php'); 
-	
-                if (isset($_POST['users'])) {
-                    $username = $_POST['user'];
-            
-                    $query = "SELECT * FROM `user_profile` WHERE `username` = '$username'"; //returns rows that have the desired date
-                    $result = $connect->query($query); //saves resultng data
+			//print the html front end for the user search box
+			echo'<div class="getTests">
+			<h1 class="labHeading">Enter a username</h1>
+			<form method="post" action="" name="users">
+			<div class="labHeading"><input type="text" name="user"  placeholder="username" id="user" required/>
+			<input type="submit" name="users" value="Resend Verification Email"></div>
+			</form>
+			</div>';
+		
+			//begin backend php code for processing of input
+			include('../../includes/connect.php'); 
+			if (isset($_POST['users'])) {
+			
+				$username = $_POST['user'];
+				$query = "SELECT * FROM `user_profile` WHERE `username` = '$username'"; //returns rows that have the desired date
+				$result = $connect->query($query); //saves resultng data
 					
-                    if($result->num_rows > 0) {
-						$row = $result->fetch_array(MYSQLI_ASSOC); //explode returned row into array
-						$UID = $row['UID'];
-						$email = $row['email'];
+				if($result->num_rows > 0) {
+					$row = $result->fetch_array(MYSQLI_ASSOC); //explode returned row into array
+					$UID = $row['UID'];
+					$email = $row['email'];
+							
+					$email_token = md5(rand(0,1000)); //prepare random # to use for new email token
+							
+					$update_email_token = "UPDATE `user_profile` SET `email_token` = '$email_token' WHERE `user_profile`.`UID` = $UID;"; //prepare sql query
+					$connect->query($update_email_token); //run sql query to update email token
 						
-						$email_token = md5(rand(0,1000)); //prepare random # to use for new email token
-						
-						$update_email_token = "UPDATE `user_profile` SET `email_token` = '$email_token' WHERE `user_profile`.`UID` = $UID;"; //prepare sql query
-						$connect->query($update_email_token); //run sql query to update email token
-						
-						//prepare and send account verification email
-						$subject = 'Verify your CTS Account';
-						$message = '
+					//prepare and send account verification email
+					$subject = 'Verify your CTS Account';
+					$message = '
 You have been registered for weekly Covid-19 Testing Services with CTS Testing Services
 ========================
 Username: ' . $username . '
@@ -95,23 +99,24 @@ Please click the following link to activate your CTS account:
 http://localhost/CovidTestingSite/includes/email_verify.php?username=' . $username . '&email_token=' . $email_token . '
 
 Thank you.
-						';
-						$headers = 'From:cts.sendmail2021@gmail.com' . "\r\n";
-						mail($email, $subject, $message, $headers); // Send our email
-						
-					} else {
-                        echo '<tr><td>There are no users with that username.</td></tr>';
-                    }
-                }
-    }
-    else{
-        echo '<h1>This page is not reachable with your level of access.</h1>';
-    }
-}catch(Exception $e){
-    echo'<h1>This page is unavailable.</h1>';
-    header('Refresh: 1;URL=../../index.php');
-}
-        ?>
+';
+					$headers = 'From:cts.sendmail2021@gmail.com' . "\r\n";
+					mail($email, $subject, $message, $headers); // Send our email		
+				} else {
+					echo '<tr><td>There are no users with that username.</td></tr>';
+				}
+			}
+		} else {
+			echo '<h1>This page is not reachable with your level of access.</h1>';
+		}
+		
+	//end try-catch block
+	//any error related to permissions, viewing variables that dont exist, etc, will result in page displaying "not available" message instead of printing the actual php code of the error message we receive
+	} catch(Exception $e){
+		echo'<h1>This page is unavailable.</h1>';
+		header('Refresh: 1;URL=../../index.php');
+	}
+?>
     </table>
     <footer>
         <div>
